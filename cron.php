f81e560d78a7532a7f86ed29afb9bf5e22c6291e
PeterTheOne <petertheone@gmail.com>
2015-08-19 10:19:04 +0200

    add robots.txt to stop crawlers
    fix #20

--- cron.php
+++ cron.php
@@ -255,6 +255,7 @@ try {
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="noindex" />
 
         <title>letterboxd-watchlist-rss - Cronjob</title>
 
--- status.php
+++ status.php
@@ -12,6 +12,7 @@ $filmsFound = $database->getFilmsOrderByCreated();
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="noindex" />
 
         <title>letterboxd-watchlist-rss - Status</title>
 
